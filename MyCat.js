ca29e7bcb50f2de2ddb7242b05348f1c86125885
Aymene <aymene01@hotmail.fr>
2020-11-18 15:57:19 +0100

    3e feature de la command cat

--- MyCat.js
+++ MyCat.js
@@ -19,5 +19,20 @@ const cat =()=>{
     }
     displayFile(process.argv[2])
     }
+
+    else if(process.argv.length == 3){
+        const filename=process.argv[2]
+        console.log(fs.readFileSync(filename, 'utf8'))
+    
+        } 
+    else if(process.argv[2] === "-e") {
+        fs.readFile(process.argv[3], 'utf8', (err, data) => {
+        if (err) throw err;
+        const lines = data.split(/\r?\n/);
+        lines.forEach((line) => {
+        console.log(line + "$");
+    });
+ });
 }
-cat()
+}
+    cat()
